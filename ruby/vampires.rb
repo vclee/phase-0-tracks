9104d4d417b47f1f5f8ff4ac2e182a82ae833ea7
Vivian Lee <the.vclee@gmail.com>
2017-06-21 18:06:24 -0700

    Create vampire check questions, and store inputs to variables.

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you? What year were you born?"
+age = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
