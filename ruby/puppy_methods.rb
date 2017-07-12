c9c1f3b4208f0e69ab1e7e2b9c6245f3b859ebf1
Vivian Lee <the.vclee@gmail.com>
2017-07-12 11:26:26 -0700

    Create puppy class, give it instance methods, test instance methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,42 @@
 class Puppy
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(num)
+    num.times do
+      puts "Woof!"
+    num
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(num)
+    puts num *= 7
+    num
+  end
+
+  def high_five
+    puts "*high fives*"
+  end
+
 end
 
+
+
+#DRIVER CODE **********************
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(4)
+fido.roll_over
+fido.dog_years(3)
+fido.high_five
+fido.initialize
