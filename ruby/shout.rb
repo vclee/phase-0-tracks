b686c83e09a47d894216144657dfbc5c815696ba
Vivian Lee <the.vclee@gmail.com>
2017-07-13 12:25:01 -0700

    Create Shout mixin module that is used by two new classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,32 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yell_happily(words)
+#     words + "!!!! :DDD"
+#   end
+# end
 
-  def self.yell_happily(words)
-    words + "!!!! :DDD"
+module Shout
+  def yell_crazily(words)
+    puts words = "!blah@#blah$%^&blah*)"
   end
 end
 
+class Me
+  include Shout
+end
+
+class You
+  include Shout
+end
+
 #DRIVER CODE ***********
-puts Shout.yell_angrily("WTF")
-puts Shout.yell_happily("WTF")
+# puts Shout.yell_angrily("WTF")
+# puts Shout.yell_happily("WTF")
+me = Me.new
+me.yell_crazily("I don't know what I'm doing!")
+
+you = You.new
+you.yell_crazily("Me neither!")
