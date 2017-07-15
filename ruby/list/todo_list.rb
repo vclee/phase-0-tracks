3d0ce23d8f5fbae741272952f6e499b0f0b5e1f8
Vivian Lee <the.vclee@gmail.com>
2017-07-14 17:32:58 -0700

    Create methods to add item, delete item, retrieve item.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -6,4 +6,17 @@ class TodoList
   def get_items
     @list
   end
+
+  def add_item(item)
+    @list.push(item)
+  end
+
+  def delete_item(item)
+    @list.delete(item) {puts "#{item} does not exist." }
+    @list
+  end
+
+  def get_item(index)
+    @list[index]
+  end
 end
