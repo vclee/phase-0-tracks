7c76526b3e2f04db59d4ab95da17653e39998dae
Vivian Lee <the.vclee@gmail.com>
2017-07-17 13:21:05 -0700

    Add function that takes in two objects, and returns true if the objects have at least one matching key-value pair.

--- js/algos.js
+++ js/algos.js
@@ -10,6 +10,20 @@ function findLongestWord(words) {
   return greatestLength;
 }
 
+function findKeyValueMatch(obj1, obj2) {
+  for (var key in obj1) {
+    for (var key in obj2) {
+      if (obj1[key] === obj2[key]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 // DRIVER CODE *********
-var longWords = ["apple", "cat", "Mississippi"];
-console.log(findLongestWord(longWords));
+// var longWords = ["apple", "cat", "Mississippi"];
+// console.log(findLongestWord(longWords));
+console.log(findKeyValueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(findKeyValueMatch({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
