8a7b0d672f9ecf9d1f702a69ee0ce773bfd23c4f
Vivian Lee <the.vclee@gmail.com>
2017-07-17 12:12:48 -0700

    Add driver code to print reversed string if condition is true.

--- js/explore.js
+++ js/explore.js
@@ -5,7 +5,7 @@
 // - Make the first letter looked at (last letter) as first letter of a new word
 // - Print the new word
 
-function reverse(word) {
+var reverse = function(word) {
   reversedWord = "";
 
   for (var i = word.length - 1; i >= 0; i--) {
@@ -15,4 +15,8 @@ function reverse(word) {
   }
 
   return reversedWord;
+}
+
+if (1 === 1) {
+  console.log(reverse("foobar"));
 }
