382fbbe877cb7b56fc48a9529c37d0162caf78b4
Vivian Lee <the.vclee@gmail.com>
2017-07-17 12:06:56 -0700

    Pseudocode how to reverse a string using JS.

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// PSEUDOCODE
+
+// - Take the word that needs to be reversed
+// - Look at each letter, starting from the last letter going backwards
+// - Make the first letter looked at (last letter) as first letter of a new word
+// - Print the new word
