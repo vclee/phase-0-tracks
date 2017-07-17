bad13f82ca8df3c7f6c40aa885c5150c9c8e1f64
Vivian Lee <the.vclee@gmail.com>
2017-07-17 12:07:39 -0700

    Write a function to reverse input string.

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,15 @@
 // - Look at each letter, starting from the last letter going backwards
 // - Make the first letter looked at (last letter) as first letter of a new word
 // - Print the new word
+
+function reverse(word) {
+  reversedWord = "";
+
+  for (var i = word.length - 1; i >= 0; i--) {
+    currentLetter = word[i];
+
+    reversedWord += currentLetter;
+  }
+
+  return reversedWord;
+}
