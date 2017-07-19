58824efb3759d4b549baf8402e5f54cd610a29f6
Vivian Lee <the.vclee@gmail.com>
2017-07-19 13:20:53 -0700

    Create constructor function for a car. Include functions and attributes.

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,19 @@ names.push("Cindy");
 console.log(colors);
 console.log(names);
 console.log(horses(colors, names));
+
+function Car(model, color, installEngine) {
+	this.model = model;
+	this.color = color;
+	this.installEngine = installEngine;
+
+	this.startCar = function() {console.log("vrooooom!");};
+}
+
+console.log("Let's build a car ...");
+var carOne = new Car("Ford", "red", true);
+console.log(carOne);
+carOne.startCar();
+var carTwo = new Car("BMW", "black", true);
+console.log(carTwo);
+carTwo.startCar();
