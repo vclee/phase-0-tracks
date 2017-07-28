b3f8834dd0bc03af130d5a9e302cce0497d8b280
Vivian Lee <the.vclee@gmail.com>
2017-07-28 14:23:43 -0700

    Fix user input questions, fix until loop, refactor. Print final contact list with style.

--- databases/contact_list/contact_list.rb
+++ databases/contact_list/contact_list.rb
@@ -29,48 +29,65 @@ if contact_list(db).empty?
     create_contact(db, Faker::Name.last_name, Faker::Name.first_name, Faker::PhoneNumber.phone_number)
   end
   contacts = db.execute("SELECT * FROM contacts")
-  # p contact_list
-else
-  contact_list(db).each do |contact|
-    puts "#{contact['last_name']}, #{contact['first_name']}, #{contact['phone_number']}, "
+end
+
+answer = true
+
+until answer == "exit"
+  puts "\nWhat would you like to do with your contacts list?"
+  puts "\nInput 'ADD' to add a new contact"
+  puts "Input 'UPDATE' to update the contact's phone number"
+  puts "Input 'REMOVE' to remove contact"
+  puts "Input 'PRINT' to print out the current contact list"
+  puts "Input 'EXIT' to exit out of program"
+  puts ""
+  answer = gets.chomp.downcase
+
+  if answer == "add"
+    puts "\nWhat is the new contact's last name?"
+    l_name = gets.chomp
+
+    puts "\nWhat is the new contact's first name?"
+    f_name = gets.chomp
+
+    puts "\nWhat is the new contact's phone number?"
+    new_num = gets.chomp
+
+    create_contact(db, l_name, f_name, new_num)
+
+  elsif answer == "update"
+    puts "\nWhat is the last name of the contact you want to modify?"
+    l_name = gets.chomp
+
+    puts "\nWhat is their new phone number?"
+    new_num = gets.chomp
+
+    found = contact_list(db).find { |contact| contact['last_name'] == l_name }
+    if found
+      db.execute("UPDATE contacts SET phone_number='#{new_num}' WHERE last_name='#{l_name}'")
+    else
+      puts "\nContact doesn't exist."
+    end
+
+  elsif answer == "remove"
+    puts "\nWhat is the last name of the contact you want to remove?"
+    l_name = gets.chomp
+    puts "\nWhat is the first name of the contact you want to remove?"
+    f_name = gets.chomp
+
+    db.execute("DELETE FROM contacts WHERE last_name='#{l_name}' AND first_name='#{f_name}'")
+
+    puts "\nContact removed."
+
+  elsif answer == "print"
+    contact_list(db).each do |contact|
+      puts "  #{contact['last_name']}, #{contact['first_name']}:   #{contact['phone_number']} "
+    end
   end
 end
 
-puts "What would you like to do with your contacts list?"
-puts "Input 'add' to add a new contact, 'update' to update the contact's phone number, or 'remove' to remove contact."
-answer = gets.chomp.downcase
-
-if answer == "add"
-  puts "what is the new contact's last name?"
-  l_name = gets.chomp
-
-  puts "what is the new contact's first name?"
-  f_name = gets.chomp
-
-  puts "what is the new contact's phone number?"
-  new_num = gets.chomp
-
-  create_contact(db, l_name, f_name, new_num)
-elsif  answer == "update"
-  puts "what is the last name of the contact you want to modify?"
-  l_name = gets.chomp
-
-  puts "what is their new phone number?"
-  new_num = gets.chomp
-
-  found = contact_list(db).find { |contact| contact['last_name'] == l_name }
-  if found
-    db.execute("UPDATE contacts SET phone_number='#{new_num}' WHERE last_name='#{l_name}'")
-  else
-    puts "Contact doesn't exist."
-  end
-elsif answer == "remove"
-  puts "what is the last name of the contact you want to remove?"
-  l_name = gets.chomp
-
-  db.execute("DELETE FROM contacts WHERE last_name='#{l_name}'")
-end
-
+puts "\n        *.*.* MY CONTACTS *.*.*"
+puts ""
 contact_list(db).each do |contact|
-  puts "#{contact['last_name']}, #{contact['first_name']}, #{contact['phone_number']}, "
+  puts "    #{contact['last_name']}, #{contact['first_name']}:    #{contact['phone_number']} "
 end
